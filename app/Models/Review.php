cac8581c8f0eff0e34e662537fc350d4e8378e48
Gyerend <gyerendlinta@gmail.com>
2025-11-30 22:56:33 +0800

    penambahan model review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
-    //
+    use HasFactory;
+
+    // PASTIKAN SEMUA FIELD INI ADA UNTUK MENGHILANGKAN MASSASSIGNMENTEXCEPTION
+    protected $fillable = [
+        'user_id',
+        'book_id',
+        'rating',
+        'comment',
+    ];
+
+    // ... (Relasi di bawah)
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function book(): BelongsTo
+    {
+        return $this->belongsTo(Book::class);
+    }
 }
